d91607048fbd2c38052f0f8a40b2c9ad5a26e6e5
Xmaw <ej222up@student.lnu.se>
2018-08-26 19:39:27 +0200

    Variable names changed and overall clean up

--- WebScraping.py
+++ WebScraping.py
@@ -23,21 +23,19 @@ character_row_2 = page_soup.findAll("tr", {"class": "row2"})
 l1 = zip(character_row_1, character_row_2)
 
 player_list = []
-for c in chain(character_row_1, character_row_2):
-    character = c.findAll("td", {"class": "rank"})
-    rank = character[0].text.strip()
+for char in chain(character_row_1, character_row_2):
+    char_container = char.findAll("td", {"class": "rank"})
+    rank = char_container[0].text.strip()
     if rank == 'Rank 3' or rank == 'Rank 2' or rank == 'Rank 1' or rank == 'Guild Master':
-        name = c.findAll("td", {"class": "name"})
+        name = char.findAll("td", {"class": "name"})
         player_list.append(name[0].text)
 
 raiders = []
 for player in player_list:
+    url = ('https://www.wowprogress.com/character/eu/draenor/')
+    url += player
 
-    raider = player
-    char_url = ('https://www.wowprogress.com/character/eu/draenor/')
-    char_url += raider
-
-    uClient = requests.get(char_url)
+    uClient = requests.get(url)
     page_html = uClient.text
     uClient.close()
 
